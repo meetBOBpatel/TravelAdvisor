54889ed68c3b08b1c5362a48d61d86c19aa65dff
Meet Patel <bobmeet1308@gmail.com>
2021-02-23 17:58:49 -0600

    Grid Menu

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,12 +11,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Home Page"),
-      ),
-      body: new Center(
-        child: Image.asset("NationalPark.jpg"),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text("Home Page"),
+        ),
+        body: 
+        GridView(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          children: <Widget>[
+            // Container(color: Colors.black, margin: EdgeInsets.all(10.0)),
+            Container(child: Image.asset("icon1.jpg"),
+            ),
+            Container(child: Image.asset("icon2.jpg")),
+            Container(child: Image.asset("icon3.jpg")),
+            Container(child: Image.asset("icon4.jpg")),
+            Container(child: Image.asset("icon5.jpg")),
+            Container(child: Image.asset("icon6.jpg")),
+            Container(child: Image.asset("icon7.jpg")),
+            Container(child: Image.asset("icon8.jpg")),
+            Container(child: Image.asset("icon9.jpg")),
+            Container(child: Image.asset("icon10.jpg")),
+            Container(child: Image.asset("icon11.jpg")),
+            Container(child: Image.asset("icon12.jpg"))
+          ],
+        ),
+
+        backgroundColor: Colors.blueGrey,
+        
+        );
+
   }
 }
