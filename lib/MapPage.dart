255284c0a2dcc9a33342224ab1701b06e8d98f90
Ali <adarawsha1@gmail.com>
2021-02-24 18:42:29 -0600

    Implemented Map within Application -Ali

--- lib/MapPage.dart
+++ lib/MapPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 // Ali's Task
 class MapPage extends StatefulWidget {
   @override
@@ -6,14 +8,27 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  GoogleMapController mapController;
+  // Holds initial position of map (in this case Yellowstone)
+  final LatLng _initialPosition = const LatLng(44.423691, -110.588516); // Yellow stone coords: 44.4280° N, 110.5885° W
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Map Page"),
       ),
-      body: new Center(
-        child: new Text("This is Map Page"),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 11.0,
+        ),
       ),
       backgroundColor: Colors.grey,
     );
