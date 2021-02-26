7dccf8b6a4cd1e429072346bb43f2d77c63551b7
VedantMajmudar <67519254+VedantMajmudar@users.noreply.github.com>
2021-02-25 21:29:03 -0600

    Update HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -17,70 +17,70 @@ class _HomePageState extends State<HomePage> {
       body: GridView(
         gridDelegate:
             SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        children: <Widget>[
+       children: <Widget>[
           // Container(color: Colors.black, margin: EdgeInsets.all(10.0)),
           Container(child: GridTile (
-                      footer: Text('Bryce Canyon', textAlign: TextAlign.center ),
+                      footer: Text('Bryce Canyon', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                     
                       child: Image.asset("assets/BryceCanyonNationalPark1.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Yosemite', textAlign: TextAlign.center),
+                      footer: Text('Yosemite', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/YosemiteNationPark.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Zion', textAlign: TextAlign.center),
+                      footer: Text('Zion', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/ZionNationalPark.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Mammoth Cave', textAlign: TextAlign.center),
+                      footer: Text('Mammoth Cave', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/MammothCaveNationalPark.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Glacier', textAlign: TextAlign.center),
+                      footer: Text('Glacier', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/GlacierNationalPark.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Grand Canyo', textAlign: TextAlign.center),
+                      footer: Text('Grand Canyo', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/GrandCanyonNationalPark.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Yellow Stone', textAlign: TextAlign.center),
+                      footer: Text('Yellow Stone', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/YellowstoneNationalPark.jpg"),
                       )
                     ),
 
           Container(child: GridTile (
-                      footer: Text('Grand Teton', textAlign: TextAlign.center),
+                      footer: Text('Grand Teton', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/GrandTetonNationalPark.jpg"),
                       )
                     ),
 
           Container(child: GridTile (
-                      footer: Text('Congaree National', textAlign: TextAlign.center),
+                      footer: Text('Congaree National', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/CongareeNationalPark.jpg"),
                       )
                     ),
           Container(child: GridTile (
-                      footer: Text('Indiana Dunes', textAlign: TextAlign.center),
+                      footer: Text('Indiana Dunes', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/IndianaDunesNationalPark.jpg"),
                       )
                     ),
           
           Container(child: GridTile (
-                      footer: Text('New River Grog', textAlign: TextAlign.center),
+                      footer: Text('New River Grog', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                       child: Image.asset("assets/NewRiverGrogeNationalPark.jpg"),
                       )
                     ),
           Container( child: GridTile
                       ( 
-                        footer: Text('Smokey Mountains', textAlign: TextAlign.center),
+                        footer: Text('Smokey Mountains', textAlign: TextAlign.center, style: TextStyle(background: Paint()..color = Colors.white),),
                         child: Image.asset("assets/SmokeyMountainNationalPark.jpg"),
                       )
                     ),
